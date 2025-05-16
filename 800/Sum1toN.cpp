87de7954655bf8ee0d2183de30691a6f4e7c2a01
RaqeebDev <raqeebauto@gmail.com>
2025-05-16 16:55:16 +0530

    Create Sum1toN.cpp

---
+++ 800/Sum1toN.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+#define fastio ios::sync_with_stdio(false); cin.tie(0);
+
+int main () {
+long long n; cin >> n ;
+long long k = n * (n+1) ;
+cout << k/2 ; 
+ 
+    
+}
