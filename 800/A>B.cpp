95cee2ca5932b9b3c161d2889c5b97fdca088da9
RaqeebDev <raqeebauto@gmail.com>
2025-05-16 17:00:02 +0530

    Update A>B.cpp

--- 800/A>B.cpp
+++ 800/A>B.cpp
@@ -3,7 +3,7 @@ using namespace std;
 #define fastio ios::sync_with_stdio(false); cin.tie(0);
 
 int main () {
-int a , b ; cin >>a,b ;
+int a , b ; cin >>a >>b ;
 if (a>=b) cout <<"Yes" ;
 else cout <<"No";
     
