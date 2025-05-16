c68644443e377ee7ec0c13ccbdae0c9ee155c6bc
RaqeebDev <raqeebauto@gmail.com>
2025-05-16 17:12:53 +0530

    Create MaxAndMin.cpp

---
+++ 800/MaxAndMin.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+#define fastio ios::sync_with_stdio(false); cin.tie(0);
+
+int main () {
+int a, b,c; cin >>a >>b >>c ;
+int largest = max(a,b,c) ;
+int smallest = min(a,b,c) ;
+
+
+
+cout <<smallest <<" " <<largest ;
+
+
+
+}
