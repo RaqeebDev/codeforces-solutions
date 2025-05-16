0ad3ef2c1e859324e252aa909135f1c982a1ead7
RaqeebDev <raqeebauto@gmail.com>
2025-05-16 16:57:32 +0530

    Create A>B.cpp

---
+++ 800/A>B.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+#define fastio ios::sync_with_stdio(false); cin.tie(0);
+
+int main () {
+int a , b ; cin >>a,b ;
+if (a>=b) cout <<"Yes" ;
+else cout <<"No";
+    
+}
