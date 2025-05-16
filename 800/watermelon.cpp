0184575230fe0008da15d6b4d3a603b8d58359c0
RaqeebDev <raqeebauto@gmail.com>
2025-05-16 16:49:18 +0530

    Create watermelon.cpp

---
+++ 800/watermelon.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+#define fastio ios::sync_with_stdio(false); cin.tie(0);
+ 
+int main () {
+int a ; cin >>a ;
+if (a%2==0 && a>2){
+    cout<<"YES";
+}
+else {
+    cout<<"NO";
+}
+ 
+ 
+ 
+    
+}
