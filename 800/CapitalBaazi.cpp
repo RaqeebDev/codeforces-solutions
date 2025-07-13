6f0e092217e53b09e3bf4bb66ced4cbf633c97a7
Raqeeb <raqeebauto@gmail.com>
2025-07-13 20:19:44 +0530

    Create CapitalBaazi.cpp

---
+++ 800/CapitalBaazi.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+
+
+   
+    
+    int n ; cin>>n;
+    string s ; cin>>s ;
+    cout <<n*2;
+    cout <<endl <<s;
+
+
+}
+
+
+
+// Github : https://github.com/RaqeebDev
+// Problem Link : https://www.hackerearth.com/problem/algorithm/jiyas-sequence/
+
+// pseudo (not organised)
+/*
+>Take Input
+>multiply n by 2 then print it.
+>Print String Directly
+*/
