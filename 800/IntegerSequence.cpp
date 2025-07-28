efb04a42fa5948ce7003f53ab7552f01ce000030
Raqeeb <raqeebauto@gmail.com>
2025-07-28 15:35:11 +0530

    Create IntegerSequence.cpp

---
+++ 800/IntegerSequence.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    long long n;
+    cin >> n;
+
+    if ((n * (n + 1) / 2) % 2 == 0)
+    {
+        cout << 0;
+    }
+    else
+    {
+        cout << 1;
+    }
+}
+
+// Github : https://github.com/RaqeebDev
+// Problem Link :https://codeforces.com/contest/1102/problem/A
+
+/* Tips and Learnings (not organised)
+
+
+*/
