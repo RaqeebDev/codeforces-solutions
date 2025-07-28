6ae0c7584a222fc261e8d746a64292a3def32bd4
Raqeeb <raqeebauto@gmail.com>
2025-07-28 16:11:13 +0530

    Create ReverseString.cpp

---
+++ 800/ReverseString.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    string s;
+    cin >> s;
+    reverse(s.begin(), s.end());
+
+    cout << s;
+}
+
+// Github : https://github.com/RaqeebDev
+// Problem Link :https://codeforces.com/contest/1102/problem/A
+
+/* Tips and Learnings (not organised)
+
+
+*/
