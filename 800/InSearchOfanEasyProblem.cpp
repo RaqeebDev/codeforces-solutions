00c4de3702a7bea5944397c0a9aebd1bd8387a99
Raqeeb <raqeebauto@gmail.com>
2025-07-29 15:58:30 +0530

    Create InSearchOfanEasyProblem.cpp

---
+++ 800/InSearchOfanEasyProblem.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int  n;
+    cin >> n;
+    int a[n];
+    for(int i = 0 ; i < n ; i++){
+        cin>>a[i];
+        if(a[i] == 1) {
+            cout << "Hard";
+            return 0;
+        }
+    }
+     cout << "Easy";
+            return 0;
+}
+
+// Github : https://github.com/RaqeebDev
+// Problem Link :https://codeforces.com/problemset/problem/1030/A
+
+/* Tips and Learnings (not organised)
+
+
+*/
